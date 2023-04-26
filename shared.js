17d287c65ae8260620e0822ddf03503790329146
Sandesh <sandeshtidake37@gmail.com>
2023-04-26 19:00:42 +0530

    linked

---
+++ shared.js
@@ -0,0 +1,9 @@
+var backdrop = document.querySelector(".backdrop");
+var option = document.querySelector(".options");
+var signup = document.getElementById("sign_up_button");
+var back = document.getElementById("back");
+
+signup.addEventListener("click", function () {
+  backdrop.style.display = "block";
+  option.style.display = "flex";
+});
