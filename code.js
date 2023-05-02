4237c7ede630f7d58b517a79375ae20560ed43cc
Sandesh <sandeshtidake37@gmail.com>
2023-05-02 12:04:23 +0530

    .

--- code.js
+++ code.js
@@ -2,7 +2,7 @@ let CODE = document.getElementById("code");
 let num = 0;
 
 function randomNum() {
-  num = Math.floor(Math.random() * 1000) + 1000;
+  num = Math.floor(Math.random() * 10000);
   console.log(num);
   CODE.innerText = num;
 }
